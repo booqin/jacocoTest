8592cdd0d1397182283fb0d0900a209472111a7e
vito <boqin2x@163.com>
2021-12-28 11:04:48 +0800

    白盒测试覆盖率实现

--- app/src/main/java/com/example/jacoco/MainActivity.java
+++ app/src/main/java/com/example/jacoco/MainActivity.java
@@ -6,11 +6,19 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private JojoDemo mJojoDemo = new JojoDemo();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.title).setOnClickListener(v -> {
+            mJojoDemo.getName();
+            mJojoDemo.dos();
+        });
+
+        findViewById(R.id.bt).setOnClickListener(v -> {
+            JacocoUtils.generateEcFile(true);
         });
     }
 }
