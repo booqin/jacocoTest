ba601081e23104984f32f767faa5608ccc09631a
vito <boqin2x@163.com>
2022-01-21 11:06:12 +0800

    jacoco auto commit

---
+++ app/src/main/java/com/example/jacoco/KDemo.kt
@@ -0,0 +1,19 @@
+package com.example.jacoco
+
+/**
+ * TODO
+ * Created by BoQin on 2022/1/21.
+ * Modified by BoQin
+ * @author BoQin
+ * @Version
+ */
+class KDemo {
+
+    fun down(){
+        println("down")
+    }
+
+    fun up(){
+        println("up")
+    }
+}
